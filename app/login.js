9d034fe909043a36ae24b4697657cef19560c34c
Jlake Ou <ouzhiwei@gmail.com>
2014-08-28 11:56:19 +0900

    add Logout button etc

--- app/login.js
+++ app/login.js
@@ -1,24 +1,25 @@
 define([
 	'require',
 	'dojo/ready',
-	'dijit/registry'
-], function(require, ready, registry){
+	'dijit/registry',
+    'app/views/LoginView',
+	'dijit/Dialog'
+], function(require, ready, registry, LoginView){
 	
 	var app = {};
 	
 	ready(function(){
-		console.log('loaded! baseUrl=' + dojoConfig.baseUrl);
+		//console.log('loaded! baseUrl=' + dojoConfig.baseUrl);
 		app.login({
 			url: dojoConfig.baseUrl + '/api/login',
 			onSuccess: function(response){
-				console.log(response.message);
+				//console.log(response.message);
 				this.onExecute(); // Hide dialog.
-				//app.alert('You are successfully logged in!');
 				location.href = dojoConfig.baseUrl + '/main.php';
 			},
 			onFailure: function(response){
 				console.log(response.message);
-				app.alert('Invalid login or password!');
+				app.alert('Invalid username or password! <br>#DEMO# username: <b>admin</b> password: <b>secret</b>');
 			}
 		});
 	});
@@ -27,20 +28,16 @@ define([
 	 * Display alert dialog.
 	 */
 	app.alert = function(message){
-		require(['dijit/Dialog'], function(Dialog){
-			var dialog = new Dialog({ title: 'Alert', content: message });
-			dialog.show();
-		});
+		var dialog = new dijit.Dialog({ title: 'Alert', content: message });
+		dialog.show();
 	};
 	
 	/**
 	 * Display login dialog.
 	 */
 	app.login = function(options){
-		require(['app/views/LoginView'], function(Dialog){
-			var dialog = new Dialog(options);
-			dialog.show();
-		});
+		var dialog = new LoginView(options);
+		dialog.show();
 	};
 	
 	return app;
--- app/views/MainView.js
+++ app/views/MainView.js
@@ -1,24 +1,33 @@
 define([
-    "dojo/_base/declare",
-    "dojo/_base/lang",
-    "dijit/_WidgetBase",
-    "dijit/_TemplatedMixin",
-    "dijit/_WidgetsInTemplateMixin",
-    "dojo/text!./templates/MainView.html",
-	"dojo/parser",
-	"dijit/layout/BorderContainer",
-	"dijit/layout/TabContainer",
-	"dijit/layout/AccordionContainer",
-	"dijit/layout/ContentPane",
-	"dijit/layout/AccordionPane"
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin',
+    'dijit/_WidgetsInTemplateMixin',
+    'dojo/text!./templates/MainView.html',
+    'dojo/parser',
+    'dijit/layout/BorderContainer',
+    'dijit/layout/TabContainer',
+    'dijit/layout/AccordionContainer',
+    'dijit/layout/ContentPane',
+    'dijit/layout/AccordionPane'
 ], function(declare, lang, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template){
     return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+
         templateString: template,
+
+        constructor: function(options){
+            lang.mixin(this, options);
+        },
+
         startup: function(){
             this.inherited(arguments);
         },
+
         postCreate: function() {
-            //alert('created!');
+            this.logoutButton.on('click', lang.hitch(this, function(e){
+                location.href = dojoConfig.baseUrl + '/logout.php';
+            }));
         }
     });
 });
